e5c8a178bc4b828e810d057af6e918328397efb9
Aisuluu <aisuluunurlan@gmail.com>
2017-03-31 18:40:41 +0600

    add header promo columns slider

---
+++ js/main.js
@@ -0,0 +1,16 @@
+ $(document).ready(function () {
+        $('.slider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true
+//            responsive: [
+//                {
+//                    breakpoint: 500,
+//                    settings: {
+//                        slidesToShow: 3,
+//                        slidesToScroll: 1,
+//                    }
+//                },
+//            ]
+});
+    });
